9780c02fd45304c49417fef4ed8e937bb03ac206
Patrick Terrematte <ptrckphilo@gmail.com.br>
2012-03-26 00:53:28 -0300

    Atualizando em função de correção no FormulaGenerator.php

--- vH/index.php
+++ vH/index.php
@@ -17,35 +17,6 @@ require_once 'FormulaGenerator.php';
 require_once 'FormulaChecker.php';
 
 
-echo "<h2> Fórmulas em Unicode</h2>";
-
-$p1 = new Atom("p1");
-$p2 = new Atom("p2");
-$p3 = new Atom("p3");
-$and = new Connective("∧","and","K",2,0);
-$or = new Connective("∨","or","A",2,0);
-$imp = new Connective("→","imp","C",2,0);
-$not = new Connective("¬","not","N",1,0);
-
-$fgenerator = new FormulaGenerator(array($and,$or,$imp,$not));
-//$fgenerator = new FormulaGenerator(array($and,$or,$imp,$not), array($p1,$p2,$p3));
-$numero_formulas = 20;
-
-$complexity_min = 4;
-$complexity_max = 4;
-
-$min_symbols = 3;
-$max_symbols = 6;
-	
-	
-for ($i = 0; $i < $numero_formulas; $i++) {
-	echo "<br/>";
-	$formula = $fgenerator->generateFormula(rand($complexity_min,$complexity_max),rand($min_symbols,$max_symbols));
-//	$formula = $fgenerator->generateFormula(rand($complexity_min,$complexity_max),rand($min_symbols,$max_symbols),array($p1,$p2,$p3));
-	echo $formula->toInfixNotation();
-}
-
-echo "<br/><br/><br/>";
 
 echo "<h2> Fórmulas na sintaxe do ProofWeb</h2>";
 
@@ -60,8 +31,7 @@ $imp = new Connective("->","imp","C",2,0);
 $bii = new Connective("<->","bii","B",2,0);
 $not = new Connective("~","not","N",1,0);
 
-$fgenerator = new FormulaGenerator(array($and,$or,$imp,$bii,$not));
-//$fgenerator = new FormulaGenerator(array($and,$or,$imp,$not), array($p1,$p2,$p3));
+$fgenerator = new FormulaGenerator(array($and,$or,$imp,$not), array($p1,$p2,$p3));
 $numero_formulas = 20;
 
 $complexity_min = 4;
@@ -74,7 +44,6 @@ $max_symbols = 6;
 for ($i = 0; $i < $numero_formulas; $i++) {
 	echo "<br/>";
 	$formula = $fgenerator->generateFormula(rand($complexity_min,$complexity_max),rand($min_symbols,$max_symbols));
-//	$formula = $fgenerator->generateFormula(rand($complexity_min,$complexity_max),rand($min_symbols,$max_symbols),array($p1,$p2,$p3));
 	echo $formula->toInfixNotation();
 }
 
@@ -84,10 +53,16 @@ echo "<br/><br/><br/>";
 echo "<h2> Fórmulas na sintaxe do Limboole</h2>";
 
 
-
-$p3 = new Atom("p");
-$p4 = new Atom("q");
-$p5 = new Atom("r");
+$p0 = new Atom("o");
+$p1 = new Atom("p");
+$p2 = new Atom("q");
+$p3 = new Atom("r");
+$p4 = new Atom("s");
+$p5 = new Atom("t");
+$p6 = new Atom("u");
+$p6 = new Atom("v");
+$p8 = new Atom("x");
+$p9 = new Atom("z");
 $and = new Connective("&","and","K",2,0);
 $or = new Connective("|","or","A",2,0);
 $imp = new Connective("->","imp","C",2,0);
@@ -95,23 +70,21 @@ $imp = new Connective("<->","bii","B",2,0);
 $not = new Connective("!","not","N",1,0);
 
 
-$fgenerator = new FormulaGenerator(array($and,$or,$imp,$not));
-//$fgenerator = new FormulaGenerator(array($and,$or,$imp,$not), array($p1,$p2,$p3));
+$fgenerator = new FormulaGenerator(array($and,$or,$imp,$not), array($p0,$p1,$p2,$p3,$p4,$p5,$p6));
 $numero_formulas = 20;
 
-$complexity_min = 4;
-$complexity_max = 4;
+$complexity_min = 25;
+$complexity_max = 30;
 
 $min_symbols = 3;
 $max_symbols = 6;
 	
+
 for ($i = 0; $i < $numero_formulas; $i++) {
 	echo "<br/>";
 	$formula = $fgenerator->generateFormula(rand($complexity_min,$complexity_max),rand($min_symbols,$max_symbols));
-//	$formula = $fgenerator->generateFormula(rand($complexity_min,$complexity_max),rand($min_symbols,$max_symbols),array($p1,$p2,$p3));
 	echo $formula->toInfixNotation();
 }
-
 
 
 echo "<br/><br/><br/>";
@@ -131,8 +104,7 @@ $imp = new Connective("\leftrightarrow","bii","B",2,0);
 $not = new Connective("\\neg","not","N",1,0);
 
 
-//$fgenerator = new FormulaGenerator(array($and,$or,$imp,$not), array($p1,$p2,$p3));
-$fgenerator = new FormulaGenerator(array($and,$or,$imp,$not));
+$fgenerator = new FormulaGenerator(array($and,$or,$imp,$not), array($p1,$p2,$p3));
 $numero_formulas = 20;
 
 $complexity_min = 4;
@@ -144,11 +116,13 @@ $max_symbols = 6;
 for ($i = 0; $i < $numero_formulas; $i++) {
 	echo "<br/>";
 	$formula = $fgenerator->generateFormula(rand($complexity_min,$complexity_max),rand($min_symbols,$max_symbols));
-//	$formula = $fgenerator->generateFormula(rand($complexity_min,$complexity_max),rand($min_symbols,$max_symbols),array($p1,$p2,$p3));
 	echo $formula->toInfixNotation();
 }
 
+
 ?>
+
+
 
 </body>
 </html>
